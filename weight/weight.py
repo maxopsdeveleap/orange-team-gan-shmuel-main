999d2e093f1b04c9957034743e7a718a8c758d14
Carmel Amarilio <carmelamarilio9@gmail.com>
2025-03-17 10:27:31 +0200

    f/create flask weight app, add health api

---
+++ weight/weight.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# @app.route("/", methods=["GET"])
+# def home():
+#     return render_template("index.html")
+
+
+@app.route("/api/health", methods=["GET"])
+def health():
+    return jsonify({"OK": "app running."}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5001")
