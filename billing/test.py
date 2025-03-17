f7c4df1a65f6ad3e436dc21d7bd4a5015a6e1387
Dor Solomon <68469554+dor-solomon@users.noreply.github.com>
2025-03-17 19:34:46 +0200

    Add basic testing script for billing

---
+++ billing/test.py
@@ -0,0 +1,48 @@
+import requests
+from app.mysqlbilling import connect
+
+link="http://127.0.0.1:5001"
+path="provider"
+id=10004
+name="Bezeq"
+
+try:
+    res = requests.post(f'{link}/{path}', json={"name":f"{name}"})
+    resp = res.json()
+    print(resp, res.status_code)
+except Exception as e:
+    print(f"Test failed with exception: {e}")
+
+try:
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Provider WHERE name = %s", (name,))
+    whole_line = cursor.fetchall()
+    for i in whole_line:
+        print(i[0], i[1])
+except Exception as e:
+    print(f"Test failed with exception: {e}")
+finally:
+    cursor.close()
+    connection.close()
+
+
+try:
+    res = requests.put(f'{link}/{path}/{id}', json={"name":f"{name}"})
+    resp = res.json()
+    print(resp, res.status_code)
+except Exception as e:
+    print(f"Test failed with exception: {e}")
+
+try:
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Provider WHERE name = %s", (name,))
+    whole_line = cursor.fetchall()
+    for i in whole_line:
+        print(i[0], i[1])
+except Exception as e:
+    print(f"Test failed with exception: {e}")
+finally:
+    cursor.close()
+    connection.close()
