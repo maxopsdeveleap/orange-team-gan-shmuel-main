115ea59e6e81fc9cd2face14f24ea7a86d4804b1
Eddy Sraiber <edi1215@gmail.com>
2025-03-19 15:08:58 +0200

    add flask cors

--- weight/app/backend/weight.py
+++ weight/app/backend/weight.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_cors import CORS
 from datetime import datetime
 import mysqlweight
 
@@ -11,6 +12,7 @@ import routes_functions.post_batch_weight
 
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route("/", methods=["GET"])
 # def home():
